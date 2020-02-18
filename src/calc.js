ac298d93450f089ec36fdf226015ccb4e9d5b6f0
April <April@Rain>
2020-02-18 23:18:47 +0000

    Full calc

--- src/calc.js
+++ src/calc.js
@@ -4,24 +4,53 @@ function calc() {
   const { materiaData } = gear;
 
   const totals = {};
+  const numerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII"];
+  const scripCosts = [0, 0, 0, 25, 200, 250, 250, 500];
 
-  data = Object.keys(materiaData).map((key) => {
+  Object.keys(materiaData).forEach((key) => {
     const [itemType, itemID] = key.split("-");
     const item = itemManager.itemList[itemID];
     const isTool = itemType.endsWith("hand");
 
-    // console.log(itemType, itemID, isTool);
-    return materiaData[key].map((m, index) => {
-      const [materia, grade] = m.split(":");
+    const isGatherer = !!gear.baseStats.GP;
+    const isCrafter = !!gear.baseStats.CP;
+
+    return materiaData[key].forEach((materia, index) => {
+      const [materiaKey, materiaGrade] = materia.split(":");
       const slot = index + 1;
       const overmeldSlots = slot - item.materiaSlots;
 
-      const meldChance = overmeldSlots <= 0 ? 100 : ToolkitData.MateriaSuccessRates[overmeldSlots - 1][grade];
-      // console.log(`Type: ${materia} Grade: ${parseInt(grade) + 1} Slot: ${slot} Chance: ${chance}`);
+      const meldChance = overmeldSlots <= 0 ? 100 : ToolkitData.MateriaSuccessRates[overmeldSlots - 1][materiaGrade];
+      const ToolMultiplier = isGatherer ? 2 : isCrafter ? 8 : 1;
+      const multiplier = isTool ? ToolMultiplier : 1;
+      const amount = (1 / (meldChance / 100)) * multiplier;
 
-      return { materia, grade, slot, meldChance, isTool };
+      const { name } = ToolkitData.Materias[materiaKey];
+
+      if (!(materia in totals)) {
+        totals[materia] = {
+          name,
+          grade: materiaGrade,
+          amount: 0,
+          scrips: 0,
+        };
+      }
+      totals[materia].amount += amount;
+      totals[materia].scrips += Math.ceil(amount) * scripCosts[materiaGrade];
     });
   });
-  console.log(data);
+  scripTotals = { 
+    white: 0,
+    yellow: 0,
+  }
+  console.log(Object.keys(totals).sort().map((key) => {
+    const { name, grade, amount, scrips } = totals[key];
+    const scripType = grade === "7" ? "white" : "yellow";
+    scripTotals[scripType] += scrips;
+    return `${name} ${numerals[grade]}: ${Math.ceil(amount)} ${scripType}: (${scrips})`;
+  }).join("\n"));
+  const { white, yellow } = scripTotals;
+  console.log(`Yellow: ${yellow.toLocaleString('en')} / White: ${white.toLocaleString('en')} / Total: ${(white + yellow).toLocaleString('en')}`)
+  
 }
 calc();
